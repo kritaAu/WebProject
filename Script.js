67dbdac7009339edad6bc160cf19932c1f2af578
kritaAu <kritapat.au@gmail.com>
2023-04-17 17:31:48 +0700

    Finis jing

--- Script.js
+++ Script.js
@@ -4,3 +4,12 @@ let navBarToggle = document.getElementById('js-toggle');
 navBarToggle.addEventListener("click",function(){
     mainNav.classList.toggle('active');
 });
+
+let popup =document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add('open-popup')
+}
+function closePopup(){
+    popup.classList.remove('open-popup')
+}
